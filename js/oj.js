32333861a1cbc5967cf78680d66914c5c3b334f6
Gaurav Kumar <gaurav.bond.itbhu@gmail.com>
2016-05-22 15:36:51 +0530

    Added PE

--- js/oj.js
+++ js/oj.js
@@ -4,6 +4,7 @@ ojApp.config(function($routeProvider) {
 	$routeProvider.when('/', {controller: ojAppCtrl, templateUrl: 'oj/uva.html'} ).//
 	when('/spoj', {controller: ojAppCtrl, templateUrl: 'oj/spoj.html'} ).//
 	when('/uva', {controller: ojAppCtrl, templateUrl: 'oj/uva.html'} ).//
+	when('/pe', {controller: ojAppCtrl, templateUrl: 'oj/pe.html'} ).//
 	otherwise({redirectTo:'/'});
 });
 
@@ -17,7 +18,8 @@ angular.module('ojServices', ['ngResource']).
 
 function ojAppCtrl($scope, $location) {
 	$scope.ojs = [ {name:'UVA', cls: ''},
-					{name: 'SPOJ', cls: ''}];
+					{name: 'SPOJ', cls: ''},
+					{name: 'Project Euler', cls: ''}];
 
 	var oldtopic= "";
 	
@@ -47,5 +49,14 @@ function SPOJListCtrl($scope, Problems) {
 
   $scope.getURL = function(id) {
   	return "https://www.spoj.com/problems/" + id;
+  }
+}
+
+function PEListCtrl($scope, Problems) {
+  $scope.problems = Problems.query({ojId: "pe"});
+  $scope.orderProp = 'id';
+
+  $scope.getURL = function(id) {
+  	return "https://projecteuler.net/problem=" + id;
   }
 }
