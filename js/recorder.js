e7aa9ea4418e78f8a880ee569084b2ee45d1e81a
Gaurav Kumar <gaurav.bond.itbhu@gmail.com>
2017-03-21 15:23:17 +0530

    .

--- js/recorder.js
+++ js/recorder.js
@@ -1,118 +1,109 @@
-/*License (MIT)
+var leftchannel = [];
+var recorder = null;
+var recording = false;
+var recordingLength = 0;
+var volume = null;
+var audioInput = null;
+var sampleRate = null;
+var audioContext = null;
+var context = null;
 
-Copyright Â© 2013 Matt Diamond
+if (!navigator.getUserMedia)
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                  navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
-the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
-to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
-the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
-THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-DEALINGS IN THE SOFTWARE.
-*/
-
-(function(window){
-
-  var WORKER_PATH = './js/recorderWorker.js';
-
-  var Recorder = function(source, cfg){
-    var config = cfg || {};
-    var bufferLen = config.bufferLen || 4096;
-    this.context = source.context;
-    if(!this.context.createScriptProcessor){
-       this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
-    } else {
-       this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
-    }
-   
-    var worker = new Worker(config.workerPath || WORKER_PATH);
-    worker.postMessage({
-      command: 'init',
-      config: {
-        sampleRate: this.context.sampleRate
-      }
+if (navigator.getUserMedia){
+    navigator.getUserMedia({audio:true}, success, function(e) {
+    alert('Error capturing audio.');
     });
-    var recording = false,
-      currCallback;
+} else alert('getUserMedia not supported in this browser.');
 
-    this.node.onaudioprocess = function(e){
-      if (!recording) return;
-      worker.postMessage({
-        command: 'record',
-        buffer: [
-          e.inputBuffer.getChannelData(0),
-          e.inputBuffer.getChannelData(1)
-        ]
-      });
-    }
+function success(e){
+    // creates the audio context
+    audioContext = window.AudioContext || window.webkitAudioContext;
+    context = new audioContext();
 
-    this.configure = function(cfg){
-      for (var prop in cfg){
-        if (cfg.hasOwnProperty(prop)){
-          config[prop] = cfg[prop];
+	// we query the context sample rate (varies depending on platforms)
+    sampleRate = context.sampleRate;
+
+    console.log(sampleRate);
+
+    console.log('succcess');
+    
+    // creates a gain node
+    volume = context.createGain();
+
+    // creates an audio node from the microphone incoming stream
+    audioInput = context.createMediaStreamSource(e);
+
+    // connect the stream to the gain node
+    audioInput.connect(volume);
+
+    var bufferSize = 4096;
+    recorder = context.createScriptProcessor(bufferSize, 1, 2);
+
+    recorder.onaudioprocess = function(e){
+        if (!recording) return;
+        var left = e.inputBuffer.getChannelData (0);
+
+        leftchannel.push.apply(leftchannel, convertoFloat32ToInt16(left));
+        recordingLength += bufferSize;
+
+        if (recordingLength == 4096*5) {
+        	recording = false;
+        	var http = new XMLHttpRequest();
+        	var url = "http://192.168.0.104:8080/zicly/hfs/";
+			http.open("POST", url, true);
+			http.setRequestHeader("Content-type", "application/json");
+
+			http.onreadystatechange = function() {//Call a function when the state changes.
+				console.log(http.readyState);
+				console.log(http.status);
+			    if(http.readyState == 4 && http.status == 200) {
+			        console.log("readyState: " + http.responseText);
+			        //recording = true;
+					leftchannel.length = 0;
+        			recordingLength = 0;        
+			    }
+			}
+			http.onload = function () {
+			    console.log(this.responseText);
+			};
+			http.send(JSON.stringify({
+			    "timestamp": Math.floor(Date.now() / 1000),
+			    "recordedData": leftchannel
+			}));
+        	console.log("leftchannel");
+        	console.log(leftchannel);
+        	
         }
-      }
     }
 
-    this.record = function(){
-      recording = true;
+    // we connect the recorder
+    volume.connect (recorder);
+    recorder.connect (context.destination); 
+}
+
+function toggleRecording( e ) {
+    if (e.classList.contains("recording")) {
+        recording = false;
+        e.classList.remove("recording");
+    } else {
+        recording = true;
+        e.classList.add("recording");
+        leftchannel.length = 0;
+        recordingLength = 0;
     }
+}
 
-    this.stop = function(){
-      recording = false;
+function convertoFloat32ToInt16(buffer) {
+    var l = buffer.length;
+    var buf = new Int16Array(l)
+
+    while (l--) {
+    	var s = Math.max(-1, Math.min(1, buffer[l]));
+    	buf[l] = s < 0 ? s * 0x8000 : s * 0x7FFF;
+        //buf[l] = buffer[l] * 0xFFFF; //convert to 16 bit
     }
-
-    this.clear = function(){
-      worker.postMessage({ command: 'clear' });
-    }
-
-    this.getBuffers = function(cb) {
-      currCallback = cb || config.callback;
-      worker.postMessage({ command: 'getBuffers' })
-    }
-
-    this.exportWAV = function(cb, type){
-      currCallback = cb || config.callback;
-      type = type || config.type || 'audio/wav';
-      if (!currCallback) throw new Error('Callback not set');
-      worker.postMessage({
-        command: 'exportWAV',
-        type: type
-      });
-    }
-
-    this.exportMonoWAV = function(cb, type){
-      currCallback = cb || config.callback;
-      type = type || config.type || 'audio/wav';
-      if (!currCallback) throw new Error('Callback not set');
-      worker.postMessage({
-        command: 'exportMonoWAV',
-        type: type
-      });
-    }
-
-    worker.onmessage = function(e){
-      var blob = e.data;
-      currCallback(blob);
-    }
-
-    source.connect(this.node);
-    this.node.connect(this.context.destination);   // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
-  };
-
-  Recorder.setupDownload = function(blob, filename){
-    var url = (window.URL || window.webkitURL).createObjectURL(blob);
-    var link = document.getElementById("save");
-    link.href = url;
-    link.download = filename || 'output.wav';
-  }
-
-  window.Recorder = Recorder;
-
-})(window);
+    return buf
+}
