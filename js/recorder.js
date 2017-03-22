9b7d4c0828ba1e769fda200de9ce098e5f46bdca
Gaurav Kumar <gaurav.bond.itbhu@gmail.com>
2017-03-22 12:28:03 +0530

    .

--- js/recorder.js
+++ js/recorder.js
@@ -61,7 +61,8 @@ function success(e) {
 			};
 			http.send(JSON.stringify({
 			    "timestamp": Math.floor(Date.now() / 1000),
-			    "recordedData": leftchannel
+			    "recordedData": leftchannel,
+			    "samplingRate": sampleRate
 			}));
 			var leftBuffer = leftchannel;
 			var view = encodeWAV(leftBuffer, true);
